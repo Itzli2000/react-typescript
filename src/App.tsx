43537ee865fe012f2924ea585686a98a56054cf2
Ariel  Molina <itzli2000@msn.com>
2021-01-21 11:45:54 -0600

    Setup: added sass handler and normalize to proyect

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,11 @@ import pizzas from './data/pizzas.json';
 function App() {
   return (
     <div className="App">
-      {
+      <ul>
+        {
         pizzas.map((pizza) => <Pizzas key={pizza.id} pizza={pizza} />)
-      }
+        }
+      </ul>
     </div>
   );
 }
